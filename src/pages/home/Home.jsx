0006302b32541391c79d4fcf7965b086378c69c3
Sixtus Innocent <sixtusinno@gmail.com>
2021-10-31 00:41:43 +0100

    Implemented the intro section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import { Divider } from "antd";
 import AppCarousel from "../../components/app-carousel/AppCarousel";
 import arrowDownIcon from "../../assets/arrow-down-icon.svg";
 
@@ -13,6 +14,20 @@ const Home = () => {
             <p>Explore our collection</p>
             <img className="Hero__explore-icon" src={arrowDownIcon} alt="" />
           </div>
+        </div>
+      </section>
+      <section className="Intro">
+        <div className="flex-row space-between align-center mb20">
+          <div className="Intro__line" />
+          <span className="heading1">Shop your style</span>
+          <div className="Intro__line" />
+        </div>
+        <div className="center Intro__text paragraph1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae gravida
+          cursus adipiscing viverra at tortor, egestas odio parturient. Morbi ut
+          lorem in erat. Et et molestie diam diam ultricies. Scelerisque duis
+          diam ac cras dictum adipiscing. Venenatis at sit proin ut vitae
+          adipiscing id facilisis.
         </div>
       </section>
     </div>
